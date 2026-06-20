d6919fa57d827a51f602e36e6fb8f2d57edc99b3
Noyon <nazibullahnoyon19.20@gmail.com>
2026-06-20 08:20:59 +0600

    update Authorize.net keys in constants file

--- lib/core/constants/authorize_net_keys.dart
+++ lib/core/constants/authorize_net_keys.dart
@@ -1,8 +1,8 @@
 // API Login ID from Authorize.net dashboard
-const authorizeNetApiLoginId = "YOUR_API_LOGIN_ID";
+const authorizeNetApiLoginId = "2QcbS63q75r";
 
 // Public Client Key from Authorize.net dashboard
-const authorizeNetPublicClientKey = "YOUR_PUBLIC_CLIENT_KEY";
+const authorizeNetPublicClientKey = "6t7C54G9XTrkpsc2Vr32jf9YB5a2vXBXjz8MCT87uD36zmNgb6dKU3Cc3xt8yJ7A";
 
 // Transaction Key from Account → Settings → API Credentials & Keys
-const authorizeNetTransactionKey = "YOUR_TRANSACTION_KEY";
+const authorizeNetTransactionKey = "435045320970";
