83f43c7c126432099a0bc16b8f55194d23065d3e
Noyon <nazibullahnoyon19.20@gmail.com>
2026-06-22 23:27:15 +0600

    update Authorize.net keys and switch to live mode for payment processing

--- lib/core/constants/authorize_net_keys.dart
+++ lib/core/constants/authorize_net_keys.dart
@@ -1,8 +1,18 @@
 // API Login ID from Authorize.net dashboard
-const authorizeNetApiLoginId = "2QcbS63q75r";
+const authorizeNetApiLoginId = "53H2UmrVwBE7";
 
 // Public Client Key from Authorize.net dashboard
-const authorizeNetPublicClientKey = "6KZscmGSPAhVL3zmVwz7wjwBWGG8Jfd7J2L4a46Wh5Xh38GyYccg3pBu4e5B2f76";
+const authorizeNetPublicClientKey = "6t7C54G9XTrkpsc2Vr32jf9YB5a2vXBXjz8MCT87uD36zmNgb6dKU3Cc3xt8yJ7A";
 
 // Transaction Key from Account → Settings → API Credentials & Keys
-const authorizeNetTransactionKey = "435045320970";
+const authorizeNetTransactionKey = "8785mPv4Vcu25C8N";
+
+
+
+// Transaction key: 8785mPv4Vcu25C8N
+
+// API Login ID:
+// 53H2UmrVwBE7
+
+// public client key
+// 6t7C54G9XTrkpsc2Vr32jf9YB5a2vXBXjz8MCT87uD36zmNgb6dKU3Cc3xt8yJ7A
