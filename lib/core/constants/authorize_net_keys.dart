2cd44502c775e52d4778b28f5a9f13611bddfc02
Nazibullah Noyon <nazibullahnoyon19.20@gmail.com>
2026-07-16 14:44:47 +0600

    update iOS dependencies, rotate Authorize.net production keys, and increment app version

--- lib/core/constants/authorize_net_keys.dart
+++ lib/core/constants/authorize_net_keys.dart
@@ -1,18 +1,20 @@
-// API Login ID from Authorize.net dashboard
-const authorizeNetApiLoginId = "453UN4sjQ2tk";
+// [Test] 
 
-// Public Client Key from Authorize.net dashboard
-const authorizeNetPublicClientKey = "9BjkjZVzH77kuN2Sr3Cfkc5jKt3B7eLPW228BE26BvMshYeBTznFh6E3c2rRyMk5";
+// // API Login ID from Authorize.net dashboard
+// const authorizeNetApiLoginId = "453UN4sjQ2tk";
 
-// Transaction Key from Account → Settings → API Credentials & Keys
-const authorizeNetTransactionKey = "7r8kpVwSA8u782WW";
+// // Public Client Key from Authorize.net dashboard
+// const authorizeNetPublicClientKey = "9BjkjZVzH77kuN2Sr3Cfkc5jKt3B7eLPW228BE26BvMshYeBTznFh6E3c2rRyMk5";
 
+// // Transaction Key from Account → Settings → API Credentials & Keys
+// const authorizeNetTransactionKey = "7r8kpVwSA8u782WW";
 
-
-// Transaction key: 8785mPv4Vcu25C8N
+/// [Production]
+const authorizeNetTransactionKey = "8785mPv4Vcu25C8N";
 
 // API Login ID:
-// 53H2UmrVwBE7
+const authorizeNetApiLoginId = "53H2UmrVwBE7";
 
 // public client key
-// 6t7C54G9XTrkpsc2Vr32jf9YB5a2vXBXjz8MCT87uD36zmNgb6dKU3Cc3xt8yJ7A
+const authorizeNetPublicClientKey =
+    "6t7C54G9XTrkpsc2Vr32jf9YB5a2vXBXjz8MCT87uD36zmNgb6dKU3Cc3xt8yJ7A";
